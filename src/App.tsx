5f4d5426e08fa18ef5e2608eca0c2b41d5acd5fa
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-13 12:25:14 +0900

    React Three Fiber - Introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./styles.css";
+import Box from "./Box";
+import { Canvas } from "@react-three/fiber";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
   );
 }
