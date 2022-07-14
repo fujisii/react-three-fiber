53358db7d942a00a1c995ca99ad776dec3e23d57
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-15 07:26:20 +0900

    Boxを1つ追加（高さ、奥域を定義）

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
+        <Box position={[0, 2, -2]} />
         <Box position={[1.2, 0, 0]} />
       </Canvas>
     </div>
